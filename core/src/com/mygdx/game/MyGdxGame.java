ac060cbc80d067114c7f7f890d84ff4a0280ed88
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2019-10-19 14:08:39 -0700

    animation example - bouncing ball

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,29 +5,48 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch sb;
-	BitmapFont font;
+	ShapeRenderer shapeRenderer;
+
+
+	static final float circleRadius = 60.0f;
+	float circleX = 200;
+	float circleY = 100;
+
+	float xSpeed = 180; // px per second
+	float ySpeed = 120; // px per second
 
 	@Override
 	public void create () {
-		sb = new SpriteBatch();
-		font = new BitmapFont();
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
+		circleX += xSpeed * Gdx.graphics.getDeltaTime();
+		circleY += ySpeed * Gdx.graphics.getDeltaTime();
+
+		if (circleX < 0 + circleRadius|| circleX > Gdx.graphics.getWidth() - circleRadius) {
+			xSpeed *= -1;
+		}
+
+		if (circleY < 0 + circleRadius || circleY > Gdx.graphics.getHeight() - circleRadius) {
+			ySpeed *= -1;
+		}
+
 		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sb.begin();
-		font.draw(sb, "Welcome to my world. Would you like to be a moderator?", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sb.end();
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(1, 1, 0.25f, 1);
+		shapeRenderer.circle(circleX, circleY, circleRadius);
+		shapeRenderer.end();
 	}
-	
+
 	@Override
 	public void dispose () {
-		sb.dispose();
-		font.dispose();
+		shapeRenderer.dispose();
 	}
 }
